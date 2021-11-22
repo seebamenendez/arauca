5a7a0e5cc523c388b5a20d9d27cc836378a0b28c
seebamenendez <seeba.menendez@gmail.com>
2021-11-22 18:21:09 -0300

    se actualiza

--- javaScript/app.js
+++ javaScript/app.js
@@ -2,8 +2,8 @@ const cards = document.getElementById('cards')
 const items = document.getElementById('items')
 const footer = document.getElementById('footer')
 const templateCard = document.getElementById('template-card').content
-//const templateFooter = document.getElementById('template-footer').content
-//const templateCarrito = document.getElementById('template-carrito').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
 const fragment = document.createDocumentFragment()
 let carrito = {}
 
@@ -61,6 +61,7 @@ const setCarrito = item => {
     
     pintarCarrito()
 }
+
 
 const pintarCarrito = () => {
     items.innerHTML = ''
